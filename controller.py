bc090db9604e46c0cfae6fa1073921e8be5928f3
Thomas Kimble <100125668+ThomasOliverKimble@users.noreply.github.com>
2022-08-18 09:39:11 +0200

    Update controller.py

--- controller.py
+++ controller.py
@@ -23,8 +23,8 @@ rightMotor.setVelocity(0.0)
 maxSpeed = min(rightMotor.getMaxVelocity(), leftMotor.getMaxVelocity())
 
 # Define the PID control constants and variables.
-KP = 50
-KI = 100
+KP = 31.4
+KI = 100-5
 KD = 0
 integral = 0.0
 previous_position = 0.0
